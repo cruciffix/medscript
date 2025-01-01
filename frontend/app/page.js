3c61b51db68859df554c4a8e2d6b28bd0e464803
Alex <mr.unnona@gmail.com>
2025-01-01 14:45:38 +0300

    first commit

---
+++ frontend/app/page.js
@@ -0,0 +1,49 @@
+'use client';
+import Image from 'next/image';
+import styles from '@/app/assets/ProgressLine.module.css';
+import Runner from './components/icons/Runner';
+import { useEffect, useState, useRef } from 'react';
+import Cup from './components/icons/Cup';
+
+export default function Home() {
+	const runnerRef = useRef(null);
+	const progressLine = useRef(null);
+	useEffect(() => {
+		function run(progress) {
+			let elem = runnerRef.current;
+			let line = progressLine.current;
+			if (elem && line) {
+				// Вычисляем длину progressLine для того, чтобы взять за 100%
+				let widthLine = 300;
+				// Вычисляем на основании progress на сколько px нам надо двинуть вперед
+				// Это обычная пропорция крест-на-крест для вычисления px по процентам progress
+				let offsetPrecent = Math.round((widthLine * progress) / 100);
+
+				// Двигаем бегунок на offsetPrecent пикселей
+				elem.style.left = offsetPrecent - elem.offsetWidth / 2 + 'px';
+				// Увеличиваем длинну progressLine в зависимости от offsetPrecent
+				line.style.width = offsetPrecent + 'px';
+			}
+		}
+
+		run(50);
+	}, []);
+	return (
+		<>
+			<div className={styles.line} ref={progressLine}>
+				<div className={styles.unerline}></div>
+
+				<div className={styles.runner} ref={runnerRef}>
+					<Runner />
+					<span className={styles.percent}>23%</span>
+				</div>
+
+				<div ref={runnerRef}>
+					<Cup />
+					<div className={styles.circle}></div>
+					<span>23%</span>
+				</div>
+			</div>
+		</>
+	);
+}
